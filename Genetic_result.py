4590e087fd4bdcf6fcf5dadf72e13c99fe347387
d99b09 <41631211+d99b09@users.noreply.github.com>
2019-01-18 17:54:26 +0300

    Create Genetic_result.py

---
+++ Genetic_result.py
@@ -0,0 +1,38 @@
+import csv
+from d_genetic import *
+from Genetic_end import *
+import time
+
+t = time.time()
+n=0
+
+
+new_list = []
+q, result= evolution(4, [0, 0, 0.35])
+FILENAME1 = "Genetic_result.csv"
+
+with open(FILENAME, "r", newline="") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        coord = list(map(float, row))
+
+        with open(FILENAME1, "a", newline="") as file:
+
+
+            q, result = d_evolution(4, coord, q)
+
+
+            if result > 0.01:
+                q, result = evolution(4, coord)
+
+            n += 1
+            print(n, result, round(time.time()+575-t,1))
+
+            new_line = coord+q
+
+            writer = csv.writer(file)
+            writer.writerow(new_line)
+
+
+
+print('Process time:', time.time()-t, 'sec')
